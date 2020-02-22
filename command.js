cb3002be10509482f1760198ddda0ed96b1476ff
euberdeveloper <euberdeveloper@gmail.com>
2020-02-22 12:31:04 +0100

    changed ip

--- command.js
+++ command.js
@@ -3,7 +3,7 @@ const mqtt = require('mqtt');
 const args = process.argv.slice(2);
 const [status, pilot, race] = args.map(n => +n);
 
-const host = '192.168.8.101';
+const host = '192.168.8.100';
 
 const client  = mqtt.connect(`mqtt://${host}`);
 client.on('connect', function () {
--- timestamp.js
+++ timestamp.js
@@ -1,7 +1,7 @@
 const mqtt = require('mqtt');
 const moment = require('moment');
 
-const host = '192.168.8.101';
+const host = '192.168.8.100';
 const timestamp = moment().format("YYYY-MM-DD HH:mm:ss.SSS");
 
 const client  = mqtt.connect(`mqtt://${host}`);
